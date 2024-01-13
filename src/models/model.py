e83b0fcde036cf945ceb4c29f9a7d005a41f64d5
Gabriellanaro <s233541@dtu.dk>
2024-01-13 18:42:57 +0100

    trying to fix model.py

--- src/models/model.py
+++ src/models/model.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from transformers import (
     T5Tokenizer,
@@ -13,6 +14,7 @@ tokenizer = T5Tokenizer.from_pretrained(MODEL)
 # tokenizer_path = "./MLOPS_final_Project/outputs/2024-01-12/13-40-09/models/2024_01_12_13_40_09/tokenizer/"
 model_path = "./MLOPS_final_Project/outputs/2024-01-12/13-40-09/models/2024_01_12_13_40_09/model"
 # tokenizer = T5Tokenizer.from_pretrained(tokenizer_path)
+print(os.listdir(model_path))
 model = T5ForConditionalGeneration.from_pretrained(model_path)
 
 

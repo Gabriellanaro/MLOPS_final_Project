6ddf5ae105ebfd774979ffaea3e5e5fd17cdf471
Gabriellanaro <s233541@dtu.dk>
2024-01-13 17:14:35 +0100

    Commented out unused code and updated model path

--- src/models/model.py
+++ src/models/model.py
@@ -8,12 +8,12 @@ from transformers import (
 
 MODEL = 't5-small'
 tokenizer = T5Tokenizer.from_pretrained(MODEL)
-model = T5ForConditionalGeneration.from_pretrained(MODEL)
+# model = T5ForConditionalGeneration.from_pretrained(MODEL)
 
 # tokenizer_path = "./MLOPS_final_Project/outputs/2024-01-12/13-40-09/models/2024_01_12_13_40_09/tokenizer/"
-# model_path = "./MLOPS_final_Project/outputs/2024-01-12/13-40-09/models/2024_01_12_13_40_09/model/"
+model_path = "/outputs/2024-01-12/13-40-09/models/2024_01_12_13_40_09/model"
 # tokenizer = T5Tokenizer.from_pretrained(tokenizer_path)
-# model = T5ForConditionalGeneration.from_pretrained(model_path)
+model = T5ForConditionalGeneration.from_pretrained(model_path)
 
 
 # Total parameters and trainable parameters.

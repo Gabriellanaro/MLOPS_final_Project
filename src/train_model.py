d10ffde0ff78b379fda01026905bb1227586913c
magolimbo / Gabriele Turetta <turetta6@gmail.com>
2024-01-15 15:48:04 +0100

    fixed hydra logs

--- src/train_model.py
+++ src/train_model.py
@@ -13,6 +13,8 @@ from torch.utils.tensorboard import SummaryWriter
 from omegaconf import OmegaConf
 import hydra
 import wandb
+import logging
+log = logging.getLogger(__name__)
 
 wandb.init(project="my-cloud-project", name="exp-hydra-cloud")
 
@@ -23,6 +25,8 @@ wandb.init(project="my-cloud-project", name="exp-hydra-cloud")
 def main(config):
     print("Load data...")
     print(f'Current working directory: {os.getcwd()}')
+    log.info("Load data...")
+    log.info(f'Current working directory: {os.getcwd()}')
 
     # Get the absolute paths using hydra.utils.to_absolute_path
     train_data_path = hydra.utils.to_absolute_path(config.data.train_data)
@@ -30,6 +34,8 @@ def main(config):
 
     print(f"train data path = {train_data_path}")
     print(f"test data path = {test_data_path}")
+    log.info(f"train data path = {train_data_path}")
+    log.info(f"test data path = {test_data_path}")
 
     with open(train_data_path, 'rb') as f:
         tokenized_train = pickle.load(f)
@@ -51,9 +57,21 @@ def main(config):
     timestamp_str = timestamp.strftime("%Y_%m_%d_%H_%M")
     OUT_DIR = f"{os.getcwd()}/models"   ## However, please note that this path will be relative. If you want to use it in subsequent file operations, you might need to convert it to an absolute path using os.path.abspath(OUT_DIR)
 
+    log.info(
+    "Set parameters...",
+    f"MODEL: {MODEL}, "
+    f"BATCH_SIZE: {BATCH_SIZE}, "
+    f"NUM_PROCS: {NUM_PROCS}, "
+    f"EPOCHS: {EPOCHS}, "
+    f"MAX_LENGTH: {MAX_LENGTH}, "
+    f"Device: {device}, "
+    f"Timestamp: {timestamp_str}, "
+    f"OUT_DIR: {OUT_DIR}"
+    )
     
     os.makedirs(f'{OUT_DIR}/{timestamp_str}', exist_ok=True)
     print(f"Results will be saved in {OUT_DIR}/{timestamp_str}")
+    log.info(f"Results will be saved in {OUT_DIR}/{timestamp_str}")
     print("Training...")
     training_args = TrainingArguments(
         output_dir=OUT_DIR,
@@ -74,6 +92,29 @@ def main(config):
         fp16=torch.cuda.is_available(),
         dataloader_num_workers=config.hp.dataloader_num_workers
     )
+
+    log.info(
+    "Training..."
+    f"Training Arguments: \n"
+    f"output_dir: {training_args.output_dir}\n"
+    f"num_train_epochs: {training_args.num_train_epochs}\n"
+    f"per_device_train_batch_size: {training_args.per_device_train_batch_size}\n"
+    f"per_device_eval_batch_size: {training_args.per_device_eval_batch_size}\n"
+    f"warmup_steps: {training_args.warmup_steps}\n"
+    f"weight_decay: {training_args.weight_decay}\n"
+    f"logging_dir: {training_args.logging_dir}\n"
+    f"logging_steps: {training_args.logging_steps}\n"
+    f"evaluation_strategy: {training_args.evaluation_strategy}\n"
+    f"save_steps: {training_args.save_steps}\n"
+    f"eval_steps: {training_args.eval_steps}\n"
+    f"load_best_model_at_end: {training_args.load_best_model_at_end}\n"
+    f"save_total_limit: {training_args.save_total_limit}\n"
+    f"report_to: {training_args.report_to}\n"
+    f"learning_rate: {training_args.learning_rate}\n"
+    f"fp16: {training_args.fp16}\n"
+    f"dataloader_num_workers: {training_args.dataloader_num_workers}\n"
+)
+
 
     print(torch.cuda.is_available())
 

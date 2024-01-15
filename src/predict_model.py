05d753a4b03465f64c99aa1c563876078712b515
Nerea <nerea.suacab@gmail.com>
2024-01-15 11:49:41 +0100

    api test

--- src/predict_model.py
+++ src/predict_model.py
@@ -36,19 +36,19 @@ def predict(input_str: str):
 
 app = FastAPI()
 
-"""@app.get("/")
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    return {"item_id": item_id}"""
+    return {"item_id": item_id}
 
 
-class Sentence(BaseModel):
+"""class Sentence(BaseModel):
     text: str
 
 @app.post("/translate/")
 async def read_sentence(sentence: Sentence):
     prediction = predict(sentence.text)
-    return {"sentence": sentence.text, "prediction": prediction}
+    return {"sentence": sentence.text, "prediction": prediction}"""

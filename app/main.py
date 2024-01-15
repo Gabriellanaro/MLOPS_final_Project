25825ea3528b48073d324b2b942fef91677b9a07
Nerea <nerea.suacab@gmail.com>
2024-01-15 11:55:15 +0100

    main test cloud

--- app/main.py
+++ app/main.py
@@ -1,27 +1,22 @@
-from http import HTTPStatus
 from fastapi import FastAPI
-from MLOPS_final_Project.src.predict_model import predict
+from http import HTTPStatus
+#from src.predict_model import predict
 from pydantic import BaseModel
 
 app = FastAPI()
-class Sentence(BaseModel):
-    text: str
 
 @app.get("/")
-def root():
-    """ Health check."""
-    response = {
-        "message": HTTPStatus.OK.phrase,
-        "status-code": HTTPStatus.OK,
-    }
-    return response
+def read_root():
+    return {"Hello": "World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
 
+"""class Sentence(BaseModel):
+    text: str
 
 @app.post("/translate/")
 async def read_sentence(sentence: Sentence):
     prediction = predict(sentence.text)
-    return {"sentence": sentence.text, "prediction": prediction}
-
-
-
+    return {"sentence": sentence.text, "prediction": prediction}"""

90910957401bf141b11e42973f39555b245a11af
Alessia Saccardo <asaccardo@fbk.eu>
2024-01-17 11:16:55 +0100

    local deployment not applicable

--- model_store/local_deployment.py
+++ model_store/local_deployment.py
@@ -4,12 +4,17 @@ import os
 
 # Specify the path to the saved model
 print(os.getcwd())
-saved_model_path = "./outputs/2024-01-12/13-40-09/models/2024_01_12_13_40_09/model"
+saved_model_path = "./outputs/model.pt"
 
 # Load the model
-model = T5ForConditionalGeneration.from_pretrained(saved_model_path)
-torch.save(model.state_dict(), f"{saved_model_path}/checkpoint.pt")
-torch.save(model.state_dict(), f"{saved_model_path}/checkpoint.pt")
-# script_model = torch.jit.script(model)
-# script_model.save('deployable_model.pt')
+MODEL = 't5-small'
+model = T5ForConditionalGeneration.from_pretrained(MODEL)
+state_dict = torch.load(saved_model_path)
+#print(state_dict.keys())
+model.load_state_dict(state_dict)
+# model = T5ForConditionalGeneration.from_pretrained(saved_model_path)
+# torch.save(model.state_dict(), f"{saved_model_path}/checkpoint.pt")
+# torch.save(model.state_dict(), f"{saved_model_path}/checkpoint.pt")
+script_model = torch.jit.script(model)
+script_model.save('deployable_model.pt')
 
